8d8b8f9e0c7ffc2a5cb1c2674c0455388d900503
Jeffrey Willis <jwillis@myocv.com>
2017-09-01 04:22:31 -0500

    Added GDD & Working on selection

--- Assets/Scripts/Managers/BattleManager.cs
+++ Assets/Scripts/Managers/BattleManager.cs
@@ -7,27 +7,33 @@ using UnityEngine.UI;
 
 public class BattleManager : MonoBehaviour{
 
-	public BattleState battleState = BattleState.playerAction;
-	public PlayerAction playerAction = PlayerAction.deciding;
-	public List<ComboStr> playerCombo = new List<ComboStr>();
-	public int numOfEnemies = 0;
-	
-	// UI Elements
+	#region UI Variables
+	//Canvas
 	private CanvasGroup OptionCanvas;
 	private CanvasGroup ComboCanvas; //Also the panel for the ComboCommands
-
-	private GameObject comboCommand;
-	
+	private CanvasGroup SelectionCanvas;
+	//Buttons
 	private Button FirstButton;
 	private Button SecondButton;
 	private Button ThirdButton;
 	private Button FourthButton;
+	//Prefabs
+	public GameObject comboCommand;
+	#endregion
+	
+	//States
+	public BattleState battleState = BattleState.playerAction;
+	public PlayerAction playerAction = PlayerAction.deciding;
+	
+	//Battle Information
+	public List<ComboStr> playerCombo = new List<ComboStr>();
+	public int numOfEnemies = 0;
 
-	public GameObject comboBar;
-
+	#region Unity Lifecycle
 	private void Awake(){
 		OptionCanvas = GameObject.Find("Options Canvas").GetComponent<CanvasGroup>();
 		ComboCanvas = GameObject.Find("Combo Canvas").GetComponent<CanvasGroup>();
+		SelectionCanvas = GameObject.Find("Selection Canvas").GetComponent<CanvasGroup>();
 		FirstButton = GameObject.Find("First Button").GetComponent<Button>();
 		FirstButton.onClick.AddListener(ActivateComboBar);
 		SecondButton = GameObject.Find("Second Button").GetComponent<Button>();
@@ -44,19 +50,9 @@ public class BattleManager : MonoBehaviour{
 	void Update () {
 		HandleState();
 	}
+	#endregion
 
-	void PositionEnemies(){
-		numOfEnemies = GameManager.instance.enemyGroup.Count;
-		String positionPrefab = "Markers/EnemyGroup" + numOfEnemies;
-		GameObject positionMarkers = Resources.Load(positionPrefab, typeof(GameObject)) as GameObject;
-		Instantiate(positionMarkers);
-		for (int i = 0; i < numOfEnemies; i++){
-			GameObject marker = GameObject.Find("Marker" + (i+1));
-			GameObject enemy = GameManager.instance.enemyGroup[i].gameObject; 
-			Instantiate(enemy, marker.transform.position, Quaternion.identity);
-		}
-	}
-
+	#region State Handling Methods
 	void HandleState(){
 		if (battleState == BattleState.playerAction){
 			HandlePlayerAction();
@@ -104,23 +100,59 @@ public class BattleManager : MonoBehaviour{
 	void SwitchState(int state){
 		battleState = (BattleState) state;
 	}
-
+	#endregion
+	
+	#region UI Methods
+	//Switches out the current menu (or Canvas Group) with another
 	void SwitchMenus(CanvasGroup currentCanvas, CanvasGroup targetCanvas){
 		UIUtils.hideCanvas(currentCanvas);
 		UIUtils.showCanvas(targetCanvas);
 
 	}
 	
+	//Switches from options menu to the combo bar
+	//Switches player state to combo
 	void ActivateComboBar(){
 		SwitchMenus(OptionCanvas, ComboCanvas);
 		playerAction = PlayerAction.combo;
 	}
 
+	//Switches from the combo bar back to the options menu
+	//Switches player state to deciding
 	void DeactivateComboBar(){
 		SwitchMenus(ComboCanvas, OptionCanvas);
 		playerAction = PlayerAction.deciding;
 	}
 	
+	
+	//[UI] Generates a new command tab for the combo bar
+	void AddCommandUI(String command){
+		GameObject commandBar = Instantiate(comboCommand, new Vector3(0, 0, 0), Quaternion.identity);
+		commandBar.transform.SetParent(ComboCanvas.transform);
+		commandBar.transform.GetChild(0).GetComponent<Text>().text = command;
+	}
+
+	//[UI] Removes last generated command tab UI
+	void RemoveCommandUI(){
+		Destroy(ComboCanvas.transform.GetChild(ComboCanvas.transform.childCount - 1).gameObject);
+	}
+	#endregion
+	
+	//This will simply place the enemies where they need
+	//to be in battle.
+	void PositionEnemies(){
+		numOfEnemies = GameManager.instance.enemyGroup.Count;
+		String positionPrefab = "Markers/EnemyGroup" + numOfEnemies;
+		GameObject positionMarkers = Resources.Load(positionPrefab, typeof(GameObject)) as GameObject;
+		Instantiate(positionMarkers);
+		for (int i = 0; i < numOfEnemies; i++){
+			GameObject marker = GameObject.Find("Marker" + (i+1));
+			GameObject enemy = GameManager.instance.enemyGroup[i].gameObject; 
+			Instantiate(enemy, marker.transform.position, Quaternion.identity);
+		}
+	}
+	
+	//Add a command to the player's combo
 	void AddComboStr(ComboStr comboStr){
 		if (playerCombo.Count != GameManager.instance.maxCombo){
 			playerCombo.Add(comboStr);
@@ -128,22 +160,15 @@ public class BattleManager : MonoBehaviour{
 		}
 	}
 
+	//Removes a command from the player's combo
 	void RemoveComboStr(){
 		playerCombo.RemoveAt(playerCombo.Count-1);
 		RemoveCommandUI();
 	}
 
-	void AddCommandUI(String command){
-		GameObject commandBar = Instantiate(comboBar, new Vector3(0, 0, 0), Quaternion.identity);
-		commandBar.transform.SetParent(ComboCanvas.transform);
-		commandBar.transform.GetChild(0).GetComponent<Text>().text = command;
-	}
-
-	void RemoveCommandUI(){
-		Destroy(ComboCanvas.transform.GetChild(ComboCanvas.transform.childCount - 1).gameObject);
-	}
 	
-	//Button Getters
+	//Gets one of the four menu buttons and adds an acttion
+	//to it's listener
 	public void AddButtonAction(int button, UnityAction action){
 		if (button == 0){
 			FirstButton.onClick.AddListener(action);
